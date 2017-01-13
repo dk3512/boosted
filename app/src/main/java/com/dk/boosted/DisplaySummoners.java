b7b80d9270c58129519b40a07c7570ab6b51210d
Daniel Kim <dkim3512@gmail.com>
2017-01-12 20:32:04 -0500

    used loops to create images and texts for players

--- app/src/main/java/com/dk/boosted/DisplaySummoners.java
+++ app/src/main/java/com/dk/boosted/DisplaySummoners.java
@@ -39,79 +39,22 @@ public class DisplaySummoners extends AppCompatActivity {
         });
 //        Log.d("CHAMPFULLS", Arrays.toString(champFulls));
 //        Log.d("SUMMFULLS", Arrays.toString(summFulls));
-        new DownloadImageTask((ImageView) findViewById(R.id.summoner1))
-                .execute("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" +
-                    champFulls[0]);
-        new DownloadImageTask((ImageView) findViewById(R.id.summoner2))
-                .execute("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" +
-                    champFulls[1]);
-        new DownloadImageTask((ImageView) findViewById(R.id.summoner3))
-                .execute("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" +
-                        champFulls[2]);
-        new DownloadImageTask((ImageView) findViewById(R.id.summoner4))
-                .execute("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" +
-                        champFulls[3]);
-        new DownloadImageTask((ImageView) findViewById(R.id.summoner5))
-                .execute("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" +
-                        champFulls[4]);
-        new DownloadImageTask((ImageView) findViewById(R.id.summoner6))
-                .execute("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" +
-                        champFulls[5]);
-        new DownloadImageTask((ImageView) findViewById(R.id.summoner7))
-                .execute("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" +
-                        champFulls[6]);
-        new DownloadImageTask((ImageView) findViewById(R.id.summoner8))
-                .execute("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" +
-                        champFulls[7]);
-        new DownloadImageTask((ImageView) findViewById(R.id.summoner9))
-                .execute("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" +
-                        champFulls[8]);
-        new DownloadImageTask((ImageView) findViewById(R.id.summoner10))
-                .execute("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" +
-                        champFulls[9]);
-        //--- text view---
-        TextView sn1 = (TextView) findViewById(R.id.summName1);
-        sn1.setText(summFulls[0]);
-        TextView sn2 = (TextView) findViewById(R.id.summName2);
-        sn2.setText(summFulls[1]);
-        TextView sn3 = (TextView) findViewById(R.id.summName3);
-        sn3.setText(summFulls[2]);
-        TextView sn4 = (TextView) findViewById(R.id.summName4);
-        sn4.setText(summFulls[3]);
-        TextView sn5 = (TextView) findViewById(R.id.summName5);
-        sn5.setText(summFulls[4]);
-        TextView sn6 = (TextView) findViewById(R.id.summName6);
-        sn6.setText(summFulls[5]);
-        TextView sn7 = (TextView) findViewById(R.id.summName7);
-        sn7.setText(summFulls[6]);
-        TextView sn8 = (TextView) findViewById(R.id.summName8);
-        sn8.setText(summFulls[7]);
-        TextView sn9 = (TextView) findViewById(R.id.summName9);
-        sn9.setText(summFulls[8]);
-        TextView sn10 = (TextView) findViewById(R.id.summName10);
-        sn10.setText(summFulls[9]);
-        TextView rank1 = (TextView) findViewById(R.id.summRank1);
-        rank1.setText(ranks[0]);
-        TextView rank2 = (TextView) findViewById(R.id.summRank2);
-        rank2.setText(ranks[1]);
-        TextView rank3 = (TextView) findViewById(R.id.summRank3);
-        rank3.setText(ranks[2]);
-        TextView rank4 = (TextView) findViewById(R.id.summRank4);
-        rank4.setText(ranks[3]);
-        TextView rank5 = (TextView) findViewById(R.id.summRank5);
-        rank5.setText(ranks[4]);
-        TextView rank6 = (TextView) findViewById(R.id.summRank6);
-        rank6.setText(ranks[5]);
-        TextView rank7 = (TextView) findViewById(R.id.summRank7);
-        rank7.setText(ranks[6]);
-        TextView rank8 = (TextView) findViewById(R.id.summRank8);
-        rank8.setText(ranks[7]);
-        TextView rank9 = (TextView) findViewById(R.id.summRank9);
-        rank9.setText(ranks[8]);
-        TextView rank10 = (TextView) findViewById(R.id.summRank10);
-        rank10.setText(ranks[9]);
+        for (int i = 1; i <= 10; i++) {
+            String imageId = "summoner" + i;
+            int resId = getResources().getIdentifier(imageId, "id", getPackageName());
+            new DownloadImageTask((ImageView) findViewById(resId))
+                    .execute("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" +
+                        champFulls[i - 1]);
+            String snId = "summName" + i;
+            int resId2 = getResources().getIdentifier(snId, "id", getPackageName());
+            TextView sn = (TextView) findViewById(resId2);
+            sn.setText(summFulls[i - 1]);
+            String rankId = "summRank" + i;
+            int resId3 = getResources().getIdentifier(rankId, "id", getPackageName());
+            TextView rank = (TextView) findViewById(resId3);
+            rank.setText(ranks[i - 1]);
+        }
     }
-// show The Image in a ImageView
 
 
 
